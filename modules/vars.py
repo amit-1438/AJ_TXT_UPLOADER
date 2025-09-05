171c61e87e0f86fa194172a55726ca3a7655ff30
amit-1438 <osmblosmauthor@gmail.com>
2025-09-06 05:20:19 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,9 +1,12 @@
 from os import getenv
 
-api_id    = int(getenv("api_id", ""))
-api_hash  = getenv("api_hash", "")
-bot_token = getenv("bot_token", "")
+api_id    = int(getenv("27502583", ""))
+api_hash  = getenv("7852db0d4ea005363ef56db3acaf4910", "")
+bot_token = getenv("7222836732:AAFeHub9L-LJeKNfp4gJagFQ8shjG7ucsHE
+
+", "")
 pwtoken = getenv("pwtoken", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-AUTH = ""
-#Bot Username = ""
+CHANNEL_ID = int(getenv("1002903892742", ""))
+AUTH = "6884122240,1124705299,5078207477"
+#Bot Username = "@Matta9211_bot"
+
